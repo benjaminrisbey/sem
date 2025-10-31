2d5f70865c52c5dfd1e807ed770544d49fdef8dc
benjamin risbey <benrisbey@gmail.com>
2025-10-31 22:20:41 +0000

    add new employee

--- src/test/java/com/napier/devops/AppIntegrationTest.java
+++ src/test/java/com/napier/devops/AppIntegrationTest.java
@@ -34,4 +34,18 @@ public class AppIntegrationTest
         Employee emp = app.getEmployee(999999);
         assertNull(emp);
     }
+
+    @Test
+    void testAddEmployee()
+    {
+        Employee emp = new Employee();
+        emp.emp_no = 500000;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        app.addEmployee(emp);
+        emp = app.getEmployee(500000);
+        assertEquals(emp.emp_no, 500000);
+        assertEquals(emp.first_name, "Kevin");
+        assertEquals(emp.last_name, "Chalmers");
+    }
 }
